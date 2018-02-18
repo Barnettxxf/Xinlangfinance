1385d288f11b377b834d270f16e13062d4cfa7a7
Barnettxxf <15102096586@163.com>
2018-02-18 12:44:00 +0800

    formatted by PEP8

--- html_downloader.py
+++ html_downloader.py
@@ -5,28 +5,28 @@ import requests
 
 
 class HtmlDownloader(object):
-    def __init__(self):
-        self.meta = False
-        self.driver = None
+        def __init__(self):
+                self.meta = False
+                self.driver = None
 
-    def downloader(self):
-        self.meta = True
-        self.driver = webdriver.PhantomJS()
+        def downloader(self):
+                self.meta = True
+                self.driver = webdriver.PhantomJS()
 
-    def download(self, url):
-        print('=======================================')
-        print('download url:', url)
-        print('meta:', self.meta)
-        self.driver = webdriver.PhantomJS()
-        if 1:
-            self.driver.get(url)
-            time.sleep(5)
-            html = self.driver.page_source
-            print('driver url', self.driver.current_url)
-        else:
-            html = requests.get(url).text
-        self.driver.close()
-        return html
+        def download(self, url):
+                print('=======================================')
+                print('download url:', url)
+                print('meta:', self.meta)
+                self.driver = webdriver.PhantomJS()
+                if 1:
+                        self.driver.get(url)
+                        time.sleep(5)
+                        html = self.driver.page_source
+                        print('driver url', self.driver.current_url)
+                else:
+                        html = requests.get(url).text
+                self.driver.close()
+                return html
 
-    def close(self):
-        self.driver.close()
+        def close(self):
+                self.driver.close()
--- spider_main.py
+++ spider_main.py
@@ -1,33 +1,35 @@
 # -*- coding=utf-8 -*-
 
 
-import url_manager, html_downloader, html_parser, outputer
+import url_manager
+import html_downloader
+import html_parser
+import outputer
 
 
 class SpiderMain(object):
-    def __init__(self, url):
-        self.urls = url_manager.UrlManager()
-        self.downloader = html_downloader.HtmlDownloader()
-        self.parser = html_parser.Parser(url)
-        self.outputer = outputer.Outputer()
-        self.root_url = url
+        def __init__(self, url):
+                self.urls = url_manager.UrlManager()
+                self.downloader = html_downloader.HtmlDownloader()
+                self.parser = html_parser.Parser(url)
+                self.outputer = outputer.Outputer()
+                self.root_url = url
 
+        def crawl(self):
+                self.outputer.creat_outputer()
+                self.urls.add_new_url(self.root_url)
 
-    def crawl(self):
-        self.outputer.creat_outputer()
-        self.urls.add_new_url(self.root_url)
-
-        while self.urls.has_new_url():
-            new_url = self.urls.get_new_url()
-            print('new_url', new_url)
-            html_cont = self.downloader.download(new_url)
-            new_urls = self.parser.parse(html_cont, new_url)
-            self.urls.add_new_urls(new_urls)
-        self.outputer.outputer()
-        print('crawl finished')
+                while self.urls.has_new_url():
+                        new_url = self.urls.get_new_url()
+                        print('new_url', new_url)
+                        html_cont = self.downloader.download(new_url)
+                        new_urls = self.parser.parse(html_cont, new_url)
+                        self.urls.add_new_urls(new_urls)
+                self.outputer.outputer()
+                print('crawl finished')
 
 
 if __name__ == '__main__':
-    root_url = 'http://finance.sina.com.cn/stock/sl/'
-    spider = SpiderMain(root_url)
-    spider.crawl()
+        root_url = 'http://finance.sina.com.cn/stock/sl/'
+        spider = SpiderMain(root_url)
+        spider.crawl()
